b9ca8ca4cb62a32b455c1b5e0f5aefc76ffec951
epetremoul <erwan.petremoul@gmail.com>
2023-05-24 13:52:08 +0200

    implementation BO

--- src/main/java/fr/eni/movielibrary/bo/Movie.java
+++ src/main/java/fr/eni/movielibrary/bo/Movie.java
@@ -9,6 +9,124 @@ public class Movie {
     private int duration;
     private String synopsis;
     private Participant director;
-    private List<Participant> actor;
-    private List<Opinion> opinion;
+    private List<Participant> actors;
+
+    private Genre genre;
+    private List<Opinion> opinions;
+
+    public Movie(long id, String title, int year, int duration, String synopsis) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+        this.synopsis = synopsis;
+    }
+
+    public Movie(long id, String title, int year, int duration, String synopsis, Participant director, List<Participant> actors, Genre genre, List<Opinion> opinion) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+        this.synopsis = synopsis;
+        this.director = director;
+        this.actors = actors;
+        this.genre = genre;
+        this.opinions = opinion;
+    }
+
+    public Movie(long id, String title, int year, int duration, String synopsis, Participant director, List<Participant> actors, List<Opinion> opinion) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+        this.synopsis = synopsis;
+        this.director = director;
+        this.actors = actors;
+        this.opinions = opinion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Participant getDirector() {
+        return director;
+    }
+
+    public void setDirector(Participant director) {
+        this.director = director;
+    }
+
+    public List<Participant> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Participant> actors) {
+        this.actors = actors;
+    }
+
+    public List<Opinion> getOpinions() {
+        return opinions;
+    }
+
+    public void setOpinions(List<Opinion> opinions) {
+        this.opinions = opinions;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "title'" + title + '\'' +
+                ", \nyear=" + year +
+                ", \nduration=" + duration +
+                ", \nsynopsis='" + synopsis + '\'' +
+                ", \ndirector=" + director +
+                ", \nactor=" + actors +
+                ", \ngenres=" + genre +
+                ", \nopinion=" + opinions +
+                '}';
+    }
 }
--- src/main/java/fr/eni/movielibrary/ihm/MovieController.java
+++ src/main/java/fr/eni/movielibrary/ihm/MovieController.java
@@ -1,9 +1,24 @@
 package fr.eni.movielibrary.ihm;
 
-import org.springframework.stereotype.Controller;
+import fr.eni.movielibrary.bll.service.MovieService;
+import fr.eni.movielibrary.bo.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Controller
+import java.util.List;
+
+@Component("movieBean")
 public class MovieController {
 
+    @Autowired
+    private MovieService movieService;
+
+    public Movie findMovie(int id) {
+        return movieService.getMovieById(id);
+    }
+
+    public List<Movie> showAllMovies() {
+        return movieService.getAllMovies();
+    }
 
 }
